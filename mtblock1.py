ace845fc19b6657e890568dd4288021dad55a1ca
ghz <100545948+gedadiao2@users.noreply.github.com>
2024-05-16 15:09:16 +0800

    Add files via upload

---
+++ mtblock1.py
@@ -0,0 +1,112 @@
+import torch.nn as nn
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size=1, stride=1, padding=0, dilation=1):
+        super(BasicConv2d, self).__init__()
+
+        self.conv = nn.Conv2d(in_planes, out_planes,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, dilation=dilation, bias=False)
+        self.gn = nn.GroupNorm(32, in_planes)
+        self.silu = nn.SiLU()
+
+    def forward(self, x):
+        x = self.gn(x)
+        x = self.silu(x)
+        x = self.conv(x)
+        return x
+class SRB(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.in_layers = nn.Sequential(
+            nn.GroupNorm(32, in_channels),
+            nn.SiLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        )
+
+        self.out_layers = nn.Sequential(
+            nn.GroupNorm(32, out_channels),
+            nn.SiLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        )
+
+        if out_channels == in_channels:
+            self.skip = nn.Identity()
+        else:
+            self.skip = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        h = self.in_layers(x)
+        h = self.out_layers(h)
+        return h + self.skip(x)
+
+class MRB(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.in_layers = nn.Sequential(
+            nn.GroupNorm(32, in_channels),
+            nn.SiLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        )
+
+        self.out_layers = nn.Sequential(
+            nn.GroupNorm(32, out_channels),
+            nn.SiLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=3, dilation=3),
+        )
+
+        if out_channels == in_channels:
+            self.skip = nn.Identity()
+        else:
+            self.skip = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        h = self.in_layers(x)
+        h = self.out_layers(h)
+        return h + self.skip(x)
+
+class LRB(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.in_layers = nn.Sequential(
+            nn.GroupNorm(32, in_channels),
+            nn.SiLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=5, padding=2),
+        )
+
+        self.out_layers = nn.Sequential(
+            nn.GroupNorm(32, out_channels),
+            nn.SiLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=5, dilation=5),
+        )
+
+        if out_channels == in_channels:
+            self.skip = nn.Identity()
+        else:
+            self.skip = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        h = self.in_layers(x)
+        h = self.out_layers(h)
+        return h + self.skip(x)
+
+class MTBlock(nn.Module):
+    def __init__(self,in_channels, out_channels):
+        super().__init__()
+        self.srb = SRB(in_channels, out_channels)
+        self.mrb = MRB(in_channels, out_channels)
+        self.lrb = LRB(in_channels, out_channels)
+        self.conv = BasicConv2d(in_channels, out_channels)
+
+    def forward(self,x):
+        x1 = self.srb(x)
+        x2 = self.mrb(x)
+        x3 = self.lrb(x)
+
+        x = x1 + x2 + x3
+
+        out1 = self.conv(x)
+        return out1
